6d093425dc4479643fe2de5d231483a59e1e23de
lassmara <lassmara1906@gmail.com>
2023-06-11 02:57:53 +0530

    projrct files

---
+++ codeproject/Assets/scar.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void scar1()
+    {
+        print("scar1");
+    }
+    public void scar2()
+    {
+        print("scar2");
+    }
+}
